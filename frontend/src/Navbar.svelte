7b20e7f74808c990ec0575224ce9f2f62788687c
jis3r <jis3r@protonmail.com>
2021-03-18 23:31:56 +0100

    fix: theme-icon inline with header on mobile

--- frontend/src/Navbar.svelte
+++ frontend/src/Navbar.svelte
@@ -28,16 +28,13 @@
 <div id="header" class="header">
     <div class="container">
         <div class="row">
-            <div class="three columns"><h6> </h6></div>
-            <div class="six columns">
+            <div class="one column theme-column"><h6> </h6></div>
+            <div class="ten columns title-column">
                 <div on:click={setLanding}>
-                    <h2 class="header-title">Planning Poker</h2>
+                    <h1 class="header-title">Planning Poker</h1>
                 </div>
             </div>
-            <div class="two columns">
-                <h6> </h6>
-            </div>
-            <div class="one column" style="height: 100%; margin-bottom: 0%;">
+            <div class="one column theme-column u-pull-right" style="height: 100%; margin-bottom: 0%;">
                 <div id="darkmodetrigger" class="darkmodetrigger u-pull-right" on:click={toggleTheme}>
                     {#if darktheme}
                     <img id="themeIcon" src="img/sun.svg" alt="sun" class="svgsun">
