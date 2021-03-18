154d20b74a47f7f879ddcddcb9cd150251ad2e1a
jis3r <jis3r@protonmail.com>
2021-03-19 00:15:47 +0100

    fix: optimized text on landingpage

--- frontend/src/Landing.svelte
+++ frontend/src/Landing.svelte
@@ -24,12 +24,11 @@
 
 <div class="row" style="margin-top: 15%; margin-bottom: 5%;">
     <div class="nine columns" style="margin-bottom: 10%;">
-        This is a simple, online planning poker tool.<br>
-        Just host a new sessions and invite your teammates to start estimating your user stories<br>
-        or join an existing session.
+        <h3>A simple, online planning poker tool.</h3>
+        <span style="font-size: 15px">Just host a session or join an existing one to start estimating your user stories.</span>        
     </div>
     <div class="three columns indexbuttons" style="margin-bottom: 10%;">
-        <button class="button-primary-start u-full-width" on:click={setStart}>Start new session</button>
-        <button class="button-primary-join u-full-width" on:click={setJoin}>Join a session</button>
+        <button class="button-primary-start u-full-width" on:click={setStart}>Start</button>
+        <button class="button-primary-join u-full-width" on:click={setJoin}>Join</button>
     </div>
 </div>
