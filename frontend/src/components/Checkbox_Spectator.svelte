fd2d0fd41fb552675cbdc9d2bd173f1142f21749
jis3r <jis3r@protonmail.com>
2021-05-25 17:06:29 +0200

    fix: static styles for spectator-checkbox

--- frontend/src/components/Checkbox_Spectator.svelte
+++ frontend/src/components/Checkbox_Spectator.svelte
@@ -19,10 +19,12 @@
 </script>
 
 <div style="cursor: pointer;" on:click={toggleSpectator}>
-    {#if isSpectator}
-    <img style="vertical-align: middle;" src="img/eye.svg" alt="checked" in:fade>
-    {:else}
-    <img style="vertical-align: middle;" src="img/eye-off.svg" alt="unchecked" in:fade>
-    {/if}
-    <span style="vertical-align: middle;">Join as spectator</span>
+    <div class="checkboxContainer">
+        {#if isSpectator}
+        <img style="vertical-align: middle;" src="img/eye.svg" alt="checked" in:fade>
+        {:else}
+        <img style="vertical-align: middle;" src="img/eye-off.svg" alt="unchecked" in:fade>
+        {/if}
+    </div>
+    <span style="padding-top: 1px; float: left;">Join as spectator</span>
 </div>
