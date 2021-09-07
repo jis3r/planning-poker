ff114b46200895823e85a16b46d5579dc77353ef
jis3r <jis3r@protonmail.com>
2021-09-07 14:56:59 +0200

    fix: optimized for-loop

--- frontend/src/utils/estimations.js
+++ frontend/src/utils/estimations.js
@@ -42,7 +42,7 @@ function setAverage(members) {
     let sum = 0;
     let count = 0;
     average = '';
-    for(let i = 0; i < members.length; i++) {
+    for(let i of members.length) {
         let estimation = members[i].estimation;
         if( estimation !== '' && estimation !== '?' && estimation !== 'coffee' ) {
             sum = sum + parseInt(members[i].estimation);
