b2e801319e4c67d4e615cfa6926a3d4cde197d72
jis3r <jis3r@protonmail.com>
2021-09-07 14:41:23 +0200

    fix: clipboard-comment

--- frontend/src/utils/clipboard.js
+++ frontend/src/utils/clipboard.js
@@ -1,6 +1,6 @@
 function copyToClipboard(content) {
     navigator.clipboard.writeText(content).then(function() {
-        //console.log('Async: Copying to clipboard was successful!');
+        //log('Async: Copying to clipboard was successful!')
     }, function(err) {
     console.error('Async: Could not copy text: ', err);
     });
